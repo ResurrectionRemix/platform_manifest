faf095f27fa03b90b19859038f1d9388788756e7
Ricardo Cerqueira <github@cerqueira.org>
2012-04-29 22:14:01 +0100

    kernel: Fix ccache inclusion logic
    Builds were broken when CCACHE was missing from the environment
    Change-Id: Ie8d6048f4600f1dc9c298593a50e37af04b96438

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -95,13 +95,9 @@ ifeq ($(TARGET_ARCH),arm)
       ccache := $(ANDROID_BUILD_TOP)/prebuilt/$(HOST_PREBUILT_TAG)/ccache/ccache
       # Check that the executable is here.
       ccache := $(strip $(wildcard $(ccache)))
-      ifdef ccache
-        ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
-        ccache = 
-      else
-        ARM_CROSS_COMPILE:=CROSS_COMPILE=$(ARM_EABI_TOOLCHAIN)/arm-eabi-
-      endif
     endif
+    ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+    ccache = 
 endif
 
 ifeq ($(TARGET_KERNEL_MODULES),)
