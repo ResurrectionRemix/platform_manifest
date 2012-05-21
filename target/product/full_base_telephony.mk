f4a2426b6103321ec89d71ba5a0f6e2d5710773b
Steve Kondik <shade@chemlab.org>
2012-05-20 22:10:30 -0700

    build: Stop installing the sample APN list
     * Sample APN list is always being installed, we don't want this in CM.
    
    Change-Id: I74b97f3af545c4a7568d3001e1435c63cfbc7de8

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -26,8 +26,5 @@ PRODUCT_PROPERTY_OVERRIDES := \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=false
 
-PRODUCT_COPY_FILES := \
-    device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
