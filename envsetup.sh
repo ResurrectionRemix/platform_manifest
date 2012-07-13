6befc1ae70ce6edb050e5dd6d447c39d04369b08
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-07-13 23:31:04 +0100

    eat: Adapt to new storage paths
    Change-Id: Ic060304275c2d19858636c84740b3f60dfdc6193

--- envsetup.sh
+++ envsetup.sh
@@ -641,7 +641,7 @@ function eat()
             echo "Device Found.."
         fi
         echo "Pushing $ZIPFILE to device"
-        if adb push $ZIPPATH /mnt/sdcard/ ; then
+        if adb push $ZIPPATH /storage/sdcard0/ ; then
             cat << EOF > /tmp/command
 --update_package=/sdcard/$ZIPFILE
 EOF
