9eb5456b71761cd96ec3acb51c3b264b46415f63
Koushik Dutta <koushd@gmail.com>
2012-07-15 15:07:07 -0700

    kernel.mk: workaround to fix kernel builds on darwin until the prebuilts/gcc is checked in by upstream.
    Change-Id: I6321fb7f6814b207c821d974766d945351b3f546

--- core/tasks/kernel.mk
+++ core/tasks/kernel.mk
@@ -108,7 +108,11 @@ ifeq ($(TARGET_ARCH),arm)
       # Check that the executable is here.
       ccache := $(strip $(wildcard $(ccache)))
     endif
-    ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+    ifeq ($(HOST_OS),darwin)
+      ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ANDROID_BUILD_TOP)/prebuilt/darwin-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-"
+    else
+      ARM_CROSS_COMPILE:=CROSS_COMPILE="$(ccache) $(ARM_EABI_TOOLCHAIN)/arm-eabi-"
+    endif
     ccache = 
 endif
 
