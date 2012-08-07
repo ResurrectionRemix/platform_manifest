4c1d258b80af138f6a6d0b68d7d6ada1d36e3f58
KhasMek <Boushh@gmail.com>
2012-08-07 19:48:48 +0100

    Remove redundant ringtone/alarm/notification defines
      All settings will now be controled in
      vendor/aokp/configs/common.mk
      for simplicities sake.
    
    Change-Id: I55be046056e5e78d7e444e203d463403188bcc05
    Signed-off-by: KhasMek <Boushh@gmail.com>
    
    Conflicts:
    	target/product/core.mk
    	target/product/mini.mk

--- target/product/core.mk
+++ target/product/core.mk
@@ -18,10 +18,6 @@ PRODUCT_BRAND := generic
 PRODUCT_DEVICE := generic
 PRODUCT_NAME := core
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.config.notification_sound=OnTheHunt.ogg \
-    ro.config.alarm_alert=Alarm_Classic.ogg
-
 PRODUCT_PACKAGES += \
     ApplicationsProvider \
     BackupRestoreConfirmation \
--- target/product/mini.mk
+++ target/product/mini.mk
@@ -47,10 +47,6 @@ PRODUCT_COPY_FILES += \
 
 #----------------- originally from core.mk ----------------
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.notification_sound=OnTheHunt.ogg \
-    ro.config.alarm_alert=Alarm_Classic.ogg
-
 # Please keep this list sorted alphabetically
 PRODUCT_PACKAGES += \
     ApplicationsProvider \
@@ -206,8 +202,6 @@ PRODUCT_PACKAGES += \
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.config.notification_sound=pixiedust.ogg
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
