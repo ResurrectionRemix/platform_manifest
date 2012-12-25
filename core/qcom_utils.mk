d152d3b08e0407910d6218b504536dd424b68dba
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2012-12-25 09:09:27 +0100

    qcom_utils: Only define QCOM_BOARD_PLATFORMS if compiling kernel inline
    	* If using prebuilt kernel KERNEL_OBJ/usr will not exist, thus
    	  build will complain of dir missing during module compilation
    
    	** Requires TARGET_USE_PREBUILT_KERNEL := true in BoardConfig for now
    
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    Change-Id: Iecd8c97e89504c60d2f01926318468ac7eb396f9

--- core/qcom_utils.mk
+++ core/qcom_utils.mk
@@ -1,5 +1,7 @@
 # Board platforms lists to be used for
 # TARGET_BOARD_PLATFORM specific featurization
+
+ifndef TARGET_USE_PREBUILT_KERNEL
 QCOM_BOARD_PLATFORMS := msm7627_surf
 QCOM_BOARD_PLATFORMS += msm7627_6x
 QCOM_BOARD_PLATFORMS += msm7627a
@@ -17,7 +19,7 @@ MSM7K_BOARD_PLATFORMS += msm7627a
 MSM7K_BOARD_PLATFORMS += msm7k
 
 QSD8K_BOARD_PLATFORMS := qsd8k
-
+endif
 
 # vars for use by utils
 empty :=
