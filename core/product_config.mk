b95acf9392db272f317bbcda61b17a2f39d95c86
altan <korukaltan@gmail.com>
2013-03-30 03:37:23 +0200

    product fix
    Change-Id: I54a21d83a555eebeb871fd66a68066c454a04993

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(AOKP_PRODUCT),)
-    all_product_configs := $(shell ls vendor/aokp/products/${AOKP_PRODUCT}.mk)
+  ifneq ($(RR_PRODUCT),)
+    all_product_configs := $(shell ls vendor/rr/products/${RR_PRODUCT}.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # AOKP_PRODUCT
+  endif # RR_PRODUCT
 endif
 
-ifeq ($(AOKP_PRODUCT),)
+ifeq ($(RR_PRODUCT),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
