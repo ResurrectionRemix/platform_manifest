c0a9cd87c570da002e39df9a29901112c4caa5cd
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-10-01 20:42:45 +0100

    tools: getb64key - print the base64 version of a PEM public key file
    Change-Id: Ia94ff560c88dfe85c3fa55db5c8219aa0d3419ee

---
+++ tools/getb64key.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import base64
+import sys
+import os
+
+pkFile = open(sys.argv[1], 'rb').readlines()
+base64Key = ""
+inCert = False
+for line in pkFile:
+    if line.startswith("-"):
+        inCert = not inCert
+        continue
+
+    base64Key += line.strip()
+
+print base64.b16encode(base64.b64decode(base64Key)).lower()
