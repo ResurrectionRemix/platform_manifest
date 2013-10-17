d1e4f9831eda9a37ed913f90b5be1333646268d1
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-10-17 20:35:05 +0530

    Telephony(MSIM) : Add telephony-msim jar to build
    Enable telephony-msim jar by adding it in PRODUCT_BOOT_JARS
     and PRODUCT_PACKAGES
    
    Change-Id: Ibc80a19d3e0c39dfbe885916d61147055f3801a9
    (cherry picked from commit b9ebc2172e9b646cb573fa5610f8a2829874b2a7)
    (cherry picked from commit 2aff65e670639f07c0962f04b65f812618a181b9)

--- target/product/core_base.mk
+++ target/product/core_base.mk
@@ -63,8 +63,9 @@ PRODUCT_PACKAGES += \
     requestsync \
     screenrecord \
     telephony-common \
-    voip-common
+    voip-common \
+    telephony-msim
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 # Override the PRODUCT_BOOT_JARS set in core_minimal.mk
-PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium
+PRODUCT_BOOT_JARS := core:conscrypt:okhttp:core-junit:bouncycastle:ext:framework:framework2:telephony-common:voip-common:mms-common:android.policy:services:apache-xml:webviewchromium:telephony-msim
