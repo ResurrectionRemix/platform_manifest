c17946fa0d10cfae2dd2dbfe33f779d1966369c8
westcripp <korukaltan@gmail.com>
2014-01-20 14:07:56 +0200

    rename zip file

--- envsetup.sh
+++ envsetup.sh
@@ -676,7 +676,7 @@ function eat()
 {
     if [ "$OUT" ] ; then
         MODVERSION=$(get_build_var CM_VERSION)
-        ZIPFILE=cm-$MODVERSION.zip
+        ZIPFILE=rr-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
             echo "Nothing to eat"
