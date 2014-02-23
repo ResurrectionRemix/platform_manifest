460572a25b037f990f95d86b5175876c1328ab68
Steve Kondik <shade@chemlab.org>
2014-02-23 23:36:22 +0100

    [HAX] Allow per-target dtbTool
     * Revert this when fixed
    
    Change-Id: I97ecb0448ae7bd5859454be290c5dde6248b2859

--- core/generate_extra_images.mk
+++ core/generate_extra_images.mk
@@ -89,7 +89,13 @@ ifeq ($(strip $(BUILD_TINY_ANDROID)),true)
 include device/qcom/common/dtbtool/Android.mk
 endif
 
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbTool$(HOST_EXECUTABLE_SUFFIX)
+ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
+DTBTOOL_NAME := dtbTool
+else
+DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
+endif
+
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
 
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
