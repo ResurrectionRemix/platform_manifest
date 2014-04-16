0024a0ccf9815af42bfdc739d68591680faa9a81
Michael Bestas <mikeioannina@gmail.com>
2014-04-16 15:08:57 +0300

    build: Disable languages that have no translations
    These languages have no translations at all and have no active maintainers
    There is no point in having them enabled.
    
    Change-Id: I2b0f6ca23ffb8c176edd38d67eddd35e4f78bc23

--- target/product/locales_full.mk
+++ target/product/locales_full.mk
@@ -1,6 +1,6 @@
 PRODUCT_LOCALES := en_US cs_CZ da_DK de_AT de_CH de_DE de_LI el_GR en_AU en_CA en_GB en_NZ en_SG eo_EU es_ES fr_CA fr_CH fr_BE fr_FR it_CH it_IT ja_JP ko_KR nb_NO nl_BE nl_NL pl_PL pt_PT ru_RU sv_SE tr_TR zh_CN zh_HK zh_TW am_ET hi_IN
 
 # CyanogenMod
-PRODUCT_LOCALES += ml_IN ku_IQ es_XA ug_CN ta_IN lb_LU
+PRODUCT_LOCALES += es_XA ug_CN ta_IN lb_LU
 
 $(call inherit-product, build/target/product/languages_full.mk)
