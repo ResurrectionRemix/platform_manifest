d4c9e8656da95aa1970cb44a1830ef73328b59fb
Michael Bestas <mikeioannina@gmail.com>
2014-05-01 16:33:02 +0300

    build: Don't build video editor libraries
    These are already defined in vendor/cm/config/common_full.mk
    We don't want them included in common_mini_* builds
    since we don't include video editor there
    
    Change-Id: I7dae9c6736590be8cd080195da4062258ee4a424

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -59,12 +59,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant
 
 PRODUCT_PACKAGES += \
-    librs_jni \
-    libvideoeditor_jni \
-    libvideoeditor_core \
-    libvideoeditor_osal \
-    libvideoeditor_videofilters \
-    libvideoeditorplayer \
+    librs_jni
 
 PRODUCT_PACKAGES += \
     audio.primary.default \
