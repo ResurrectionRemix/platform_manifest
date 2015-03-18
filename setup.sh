c19f6ec4c4d5613fcf5febf44492905ad50cfc8e
Furrydaus <firdaus1999.amin@gmail.com>
2015-03-18 08:57:47 +0800

    Fixed Typo

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ sleep 1
 echo Installing Dependencies in 1 Seconds...
 sleep 1
 sudo apt-get update
-+sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-6-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib openjdk-7-jdk android-tools-adb android-tools-fastboot liblz4-
+sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-6-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib openjdk-7-jdk android-tools-adb android-tools-fastboot liblz4-
 y
 clear
 echo Dependencies have been installed!
