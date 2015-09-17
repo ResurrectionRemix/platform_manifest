02a4029a554e830084f4798e379aed0c0989a2f4
Pranav Reddy <pranavreddy89@yahoo.in>
2015-09-17 22:44:56 +0530

    Few Needed Packages!

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ sleep 1
 sudo apt-get update
 sudo apt-get install openjdk-7-jdk
 y
-sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-6-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib
+sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-7-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib python pngquant liblz4-*
 y
 clear
 echo Dependencies have been installed!
