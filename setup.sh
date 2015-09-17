e29dc41db1ff542f249a1817b6046bdc57dad51d
Pranav Reddy <pranavreddy89@yahoo.in>
2015-09-17 22:37:53 +0530

    Few Changes!

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ sleep 1
 sudo apt-get update
 sudo apt-get install openjdk-7-jdk
 y
-sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-6-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib
+sudo apt-get update && sudo apt-get install git-core gnupg flex bison gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools build-essential zip curl libncurses5-dev zlib1g-dev openjdk-6-jre openjdk-6-jdk pngcrush schedtool libxml2 libxml2-utils xsltproc lzop libc6-dev schedtool g++-multilib lib32z1-dev lib32ncurses5-dev lib32readline-gplv2-dev gcc-multilib python liblz4-* pngquant
 y
 clear
 echo Dependencies have been installed!
@@ -96,8 +96,8 @@ sleep 3
                 echo Enter path to directory you want to use for ccache.
                 echo Something like /home/$USER/.ccache
                 read ccpath
-                export USE_CCACHE=1
-                export CCACHE_DIR=$ccpath/
+                export USE_CCACHE=1 >> ~/.bashrc
+                export CCACHE_DIR=$ccpath >> ~/.bashrc
                 prebuilts/misc/linux-x86/ccache/ccache -M $ccsize
         sleep 3
         clear
